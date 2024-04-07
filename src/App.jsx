66159cbc74cee92b73f4af18eff24c7a681b0f8c
Tashika Wijesooriya <it22644244@my.sliit.lk>
2024-04-07 16:24:45 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import createSalesOrder from './pages/createSalesOrder';
-import deleteSalesOrders from './pages/deleteSalesOrders';
-import pendingOrders from './pages/pendingOrders';
-import selesOrderDetails from './pages/salesOrderDetails';
-import selesOrders from './pages/salesOrders';
-import updatePendingSelesOrders from './pages/updatePendingSelesOrders';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CreateSalesOrder from './pages/createSalesOrder';
+import DeleteSalesOrders from './pages/deleteSalesOrders';
+import PendingOrders from './pages/pendingOrders';
+import SelesOrderDetails from './pages/salesOrderDetails';
+import SelesOrders from './pages/salesOrders';
+import UpdatePendingSelesOrders from './pages/updatePendingSelesOrders';
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+
 
 export default function App() {
-  return (<BrowserRouter>
-    <Routes>
-      <Route path ="/" element={<createSalesOrder/>}/>
-      <Route path ="/delete-sales-order" element={<deleteSalesOrders />}/>
-      <Route path ="/pending-orders" element={<pendingOrders />}/>
-      <Route path ="/sales-order-details" element={<salesOrderDetails/>}/>
-      <Route path ="/sales-orders" element={<salesOrders/>}/>
-      <Route path ="/update-pending-orders" element={<updatePendingSelesOrders/>}/>
-  
-      </Routes></BrowserRouter>)
+  return (
+    <BrowserRouter>
+    <Header></Header>
+    <Dashboard/>
+      <Routes>
+        <Route path="/" element={<CreateSalesOrder />} />
+        <Route path="/delete-sales-order" element={<DeleteSalesOrders />} />
+        <Route path="/pending-orders" element={<PendingOrders />} />
+        <Route path="/sales-order-details" element={<SelesOrderDetails />} />
+        <Route path="/sales-orders" element={<SelesOrders />} />
+        <Route path="/update-pending-orders" element={<UpdatePendingSelesOrders />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
