b88786e18e25ef680b27a513692cf82b7968a8bd
Tashika Wijesooriya <it22644244@my.sliit.lk>
2024-04-07 14:10:17 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import createSalesOrder from './pages/createSalesOrder';
+import deleteSalesOrders from './pages/deleteSalesOrders';
+import pendingOrders from './pages/pendingOrders';
+import selesOrderDetails from './pages/salesOrderDetails';
+import selesOrders from './pages/salesOrders';
+import updatePendingSelesOrders from './pages/updatePendingSelesOrders';
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return (<BrowserRouter>
+    <Routes>
+      <Route path ="/" element={<createSalesOrder/>}/>
+      <Route path ="/delete-sales-order" element={<deleteSalesOrders />}/>
+      <Route path ="/pending-orders" element={<pendingOrders />}/>
+      <Route path ="/sales-order-details" element={<salesOrderDetails/>}/>
+      <Route path ="/sales-orders" element={<salesOrders/>}/>
+      <Route path ="/update-pending-orders" element={<updatePendingSelesOrders/>}/>
+  
+      </Routes></BrowserRouter>)
 }
